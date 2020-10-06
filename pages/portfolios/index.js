d171143a535827092e1d17437c0372e965aa2347
JasonDollar <elrey33@gmail.com>
2020-10-06 12:48:05 +0200

    PortfolioCard component

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import axios from 'axios'
 
+import PortfolioCard from '../../components/portfolios/PortfolioCard'
+
 const fetchPortfolios = () => {
   const query = `
     query Portfolios {
@@ -33,42 +35,11 @@ const Portfolios = ({ portfolios }) => (
     </section>
     <section className="pb-5">
       <div className="row">
-        <div className="col-md-4">
-          <div className="card subtle-shadow no-border">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-              <p className="card-text fs-2">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <div className="card-footer no-border">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </div>
+        {portfolios.map(item => (
+          <div key={item._id} className="col-md-4">
+            <PortfolioCard portfolio={item} />
           </div>
-        </div>
-        <div className="col-md-4">
-          <div className="card subtle-shadow no-border">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-              <p className="card-text fs-2 ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <div className="card-footer no-border">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="card subtle-shadow no-border">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-              <p className="card-text fs-2 ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <div className="card-footer no-border">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   </>

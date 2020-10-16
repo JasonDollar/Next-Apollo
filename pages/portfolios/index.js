f3c683dcaad24fa9857232f11e81c128c1d46148
JasonDollar <elrey33@gmail.com>
2020-10-16 18:06:28 +0200

    update and delete portfolio, refactor

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -5,63 +5,23 @@ import { useQuery, useMutation } from '@apollo/client'
 import { getDataFromTree } from '@apollo/react-ssr'
 import withApollo from '../../hoc/withApollo'
 
-import { GET_PORTFOLIOS, CREATE_PORTFOLIO, GET_PORTFOLIO } from '../../apollo/queries'
+import {   
+  useGetPortfolios,
+  useUpdatePortfolio,
+  useDeletePortfolio,
+  useCreatePortfolio 
+} from '../../apollo/actions'
 
 import PortfolioCard from '../../components/portfolios/PortfolioCard'
 
 
-const graphUpdatePortfolio = id => {
-  const query = `
-    mutation UpdatePortfolio {
-      updatePortfolio(id: "${id}",input: {
-        title: "UPDATE Job"
-        company: "UPDATE Company"
-        companyWebsite: "UPDATE Website"
-        location: "UPDATE Location"
-        jobTitle: "UPDATE Job Title"
-        description: "UPDATE Desc"
-        startDate: "12/12/2012 UPDATE"
-        endDate: "14/11/2013 UPDATE"
-      }) {
-        _id,
-        title,
-        company,
-        companyWebsite
-        location
-        jobTitle
-        description
-        startDate
-        endDate
-      }
-    }`
-  return axios.post('http://localhost:3000/graphql', { query })
-    .then(({ data: graph }) => graph.data)
-    .then(data => data.updatePortfolio)
-}
-
-const graphDeletePortfolio = id => {
-  const query = `
-    mutation DeletePortfolio {
-      deletePortfolio(id: "${id}")
-    }
-  `
-
-  return axios.post('http://localhost:3000/graphql', { query })
-    .then(({ data: graph }) => graph.data)
-    .then(data => data.deletePortfolio)
-}
 
 const Portfolios = () => {
   const [portfolios, setPortfolios] = useState([])
-  const { loading, error, data } = useQuery(GET_PORTFOLIOS)
-  const [createPortfolio, { data: dataC }] = useMutation(CREATE_PORTFOLIO, {
-    update: (cache, { data: { createPortfolio } }) => {
-      console.log(createPortfolio)
-      const { portfolios } = cache.readQuery({ query: GET_PORTFOLIOS })
-      console.log(portfolios)
-      cache.writeQuery({ query: GET_PORTFOLIOS, data: { portfolios: [...portfolios, createPortfolio] } })
-    },
-  })
+  const { data } = useGetPortfolios();
+  const [ updatePortfolio ] = useUpdatePortfolio();
+  const [ deletePortfolio ] = useDeletePortfolio();
+  const [ createPortfolio ] = useCreatePortfolio();
   
 
 
@@ -69,26 +29,7 @@ const Portfolios = () => {
     setPortfolios(data.portfolios)
   }
 
-  // if (loading) return <div>Loading</div>
-  // if (error) return <div>Error</div>
 
-
-
-  const updatePortfolio = async id => {
-    const updatedPortfolio = await graphUpdatePortfolio(id)
-    const index = portfolios.findIndex(p => p._id === id)
-    const newPortfolios = portfolios.slice()
-    newPortfolios[index] = updatedPortfolio
-    setPortfolios(newPortfolios)
-  }
-
-  const deletePortfolio = async id => {
-    const deletedId = await graphDeletePortfolio(id)
-    const index = portfolios.findIndex(p => p._id === deletedId)
-    const newPortfolios = portfolios.slice()
-    newPortfolios.splice(index, 1)
-    setPortfolios(newPortfolios)
-  }
   
   return (
     <>
@@ -119,11 +60,11 @@ const Portfolios = () => {
               </Link>
               <button 
                 className="btn btn-warning"
-                onClick={() => updatePortfolio(item._id)}
+                onClick={() => updatePortfolio({variables: {id: item._id}})}
               >Update
               </button>
               <button
-                onClick={() => deletePortfolio(item._id)}
+                onClick={() => deletePortfolio({variables: {id: item._id}})}
                 className="btn btn-danger"
               >
                 Delete Portfolio
